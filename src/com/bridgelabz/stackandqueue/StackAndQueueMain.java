0d69b7774a66820e3991527575644a7a888e94d5
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 21:57:51 +0530

    ADDED:class the provides main method

---
+++ src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.stackandqueue;
+
+import com.bridgelabz.linkedlist.MyNode;
+
+public class StackAndQueueMain {
+	public static void main(String[] args) {
+		MyStack<Integer> myStack = new MyStack<Integer>();
+		
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		
+		myStack.push(firstNode);
+		myStack.push(secondNode);
+		myStack.push(thirdNode);
+		
+		System.out.println("The content of stack starting from top : ");
+		myStack.display();
+	}
+}
