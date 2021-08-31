32d34bddc1cd2bb2415ea6a692af8c17c1696b07
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 22:17:39 +0530

    ADDED: to demonstrate stack operations

--- src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
+++ src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
@@ -17,5 +17,16 @@ public class StackAndQueueMain {
 		System.out.println("The content of stack starting from top : ");
 		myStack.display();
 		
+		System.out.println("the size of the stack : "+myStack.size());
+		
+		System.out.println("The popped element is : "+myStack.pop().getKey());
+		
+		System.out.println("The content of stack starting from top : ");
+		myStack.display();
+		
+		System.out.println("the element at top :"+ myStack.peek().getKey());
+		
+		System.out.println("is stack empty : "+ myStack.isEmpty());
+		
 	}
 }
