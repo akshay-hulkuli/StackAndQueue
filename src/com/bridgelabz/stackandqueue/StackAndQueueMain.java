f34351d2113f7e0b1e22c4538eaf04d4a137c0fd
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 22:38:03 +0530

    REFACTORED: to demonstrate all queue operations

--- src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
+++ src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
@@ -6,18 +6,26 @@ public class StackAndQueueMain {
 	public static void main(String[] args) {
 		MyQueue<Integer> myQueue = new MyQueue<Integer>();
 		
-		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
 		
 		myQueue.enqueue(firstNode);
 		myQueue.enqueue(secondNode);
 		myQueue.enqueue(thirdNode);
 		
-		System.out.println("The content of Queue starting from top : ");
+		System.out.println("The content of queue starting from rare end : ");
+		myQueue.display();
+		
+		System.out.println("the size of the queue : "+myQueue.size());
+		
+		System.out.println("The dequeued element is : "+myQueue.dequeue().getKey());
+		
+		System.out.println("The content of queue starting from rare end: ");
 		myQueue.display();
 		
 		
+		System.out.println("is queue empty : "+ myQueue.isEmpty());
 		
 	}
 }
