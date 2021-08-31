7ac4edfc4797a4f89750ede8b8749f707e62b240
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-31 22:28:18 +0530

    ADDED: to demonstrate queue operations

--- src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
+++ src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
@@ -4,29 +4,20 @@ import com.bridgelabz.linkedlist.MyNode;
 
 public class StackAndQueueMain {
 	public static void main(String[] args) {
-		MyStack<Integer> myStack = new MyStack<Integer>();
+		MyQueue<Integer> myQueue = new MyQueue<Integer>();
 		
-		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
 		
-		myStack.push(firstNode);
-		myStack.push(secondNode);
-		myStack.push(thirdNode);
+		myQueue.enqueue(firstNode);
+		myQueue.enqueue(secondNode);
+		myQueue.enqueue(thirdNode);
 		
-		System.out.println("The content of stack starting from top : ");
-		myStack.display();
+		System.out.println("The content of Queue starting from top : ");
+		myQueue.display();
 		
-		System.out.println("the size of the stack : "+myStack.size());
 		
-		System.out.println("The popped element is : "+myStack.pop().getKey());
-		
-		System.out.println("The content of stack starting from top : ");
-		myStack.display();
-		
-		System.out.println("the element at top :"+ myStack.peek().getKey());
-		
-		System.out.println("is stack empty : "+ myStack.isEmpty());
 		
 	}
 }
